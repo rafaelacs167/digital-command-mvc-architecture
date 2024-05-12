23a5e6efb519c4473a8f5d82e28f0fa4372af6e8
rafael alves <rafaelc.santos@sicoob.com.br>
2024-05-11 22:26:30 -0300

    criar projeto

---
+++ src/main/java/com/digital/command/DigitalCommandMvcArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.digital.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalCommandMvcArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalCommandMvcArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/command/DigitalCommandMvcArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalCommandMvcArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
